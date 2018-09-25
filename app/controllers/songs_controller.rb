e04e0a44acaf0ee0215addbe2f028aeb224b9ec6
Alex Zdatny <alexzdatny@gmail.com>
2018-09-25 18:16:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,6 +3,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist
+        @songs = @artist.songs
     else
       @songs = Song.all
     end
